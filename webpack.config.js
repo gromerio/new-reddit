688636e099a4b5936be3d41d0a95cbdae65b547d
gromerio <101283524+gromerio@users.noreply.github.com>
2022-03-11 14:06:00 +0300

    lesson 2.1
    18:00min
    Error  TS1005: ',' expected.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 const NODE_ENV = process.env.NODE_ENV
 
 module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+    },
     mode: NODE_ENV ? NODE_ENV : 'development',
     entry: path.resolve(__dirname, 'src/index.jsx'),
     output: {
